215f9bbda8ce83080cc7aaf4d3d651b85f8ee5d9
xmcc <596341028@qq.com>
2019-04-15 19:22:41 +0800

    GIT测试，第一次提交

---
+++ src/main/java/com/xmcc/wxsell/WxSellApplication.java
@@ -0,0 +1,13 @@
+package com.xmcc.wxsell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxSellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxSellApplication.class, args);
+    }
+
+}
